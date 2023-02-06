e8c4a10f9a799e0f480e7899456fd87038eba69a
Michal Bereznicki <michalbereznicki121345@gmail.com>
2023-02-06 15:26:53 +0000

    Initial commit

---
+++ Other side sripts/Optimize_Mesh.py
@@ -0,0 +1,16 @@
+import maya.cmds as cmds
+import maya.mel as mel
+
+def optimizeEdges(Object:str,lowA:int,HighA:int):
+    cmds.selectMode(object=True)
+    cmds.polyListComponentConversion(Object,te=True)
+    cmds.polySelectConstraint( m=3, t=0x8000, a=True, ab=(lowA, HighA) )
+    HardEdges = cmds.ls(sl=True)
+    cmds.polySelectConstraint(m=0, a=False)
+    cmds.select(HardEdges)
+    mel.eval("GrowLoopPolygonSelectionRegion;SelectEdgeLoopSp;InvertSelection;")
+    cmds.polyDelEdge(cmds.ls(sl=True),cv=True)
+
+if __name__ == "__main__":
+    for i in cmds.ls(sl=True):
+        optimizeEdges(i,45,91)
