1d7a772f0502a075fc143607915179a5d6723839
Michal Bereznicki <michal212345@users.noreply.github.com>
2023-04-03 18:02:28 +0100

    Fixed no image issue

--- Other side sripts/SmartTransparency.py
+++ Other side sripts/SmartTransparency.py
@@ -4,7 +4,7 @@ try:
 except:
     cmds.error("You need to install Pillow using Mayapy.")
 
-def has_transparency(img):
+def has_transparency(img:Image.Image):
     if img.info.get("transparency", None) is not None:
         return True
     if img.mode == "P":
@@ -20,8 +20,11 @@ def has_transparency(img):
     return False
 
 for i in cmds.ls(sl=True):
-    if len(cmds.listConnections(i+".color")) == 1:
-        file = cmds.listConnections(i+".color")[0]
-        filePath = cmds.getAttr(file+".fileTextureName")
-        if has_transparency(Image.open(filePath)):
-            cmds.connectAttr(file+".outTransparency",i+".transparency")
+    try:
+        if len(cmds.listConnections(i+".color")) == 1:
+            file = cmds.listConnections(i+".color")[0]
+            filePath = cmds.getAttr(file+".fileTextureName")
+            if has_transparency(Image.open(filePath)):
+                cmds.connectAttr(file+".outTransparency",i+".transparency")
+    except:
+        continue
